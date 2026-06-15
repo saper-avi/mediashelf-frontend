d01190728dc774598777014951305b1114069ab9
saper-avi <hahh20200@gmail.com>
2026-06-15 09:28:44 +0500

    feat: update navbar with search bar

--- mediashelf-frontend/src/components/NavBar/NavBar.tsx
+++ mediashelf-frontend/src/components/NavBar/NavBar.tsx
@@ -3,11 +3,20 @@ export default function NavBar() {
         <nav className="flex justify-between items-center p-4 text-white">
             <p className="text-2xl font-bold">MediaShelf</p>
             <div className="flex gap-4">
-                <p className="cursor-pointer hover:text-teal-400">Поиск</p>
-                <p className="cursor-pointer hover:text-teal-400">Мой список</p>
+                <p className="cursor-pointer hover:text-teal-400">Главная</p>
+                <p className="cursor-pointer hover:text-teal-400">Избранные</p>
             </div>
+            <div className = "flex items-center gap-2 bg-white/10 rounded-full px-4 py-2">
+<input 
+      placeholder="Поиск"
+      className="bg-transparent outline-none text-white placeholder-gray-400"
+      />
+      <button className="text-white">🔍</button>
+</div>
+
+
         </nav>
+        
     )
 }
-
 
